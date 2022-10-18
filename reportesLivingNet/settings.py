c9e45f344beac0c4834c2fc12124499ea86484cd
bryamozl <bryan95_19@hotmail.com>
2022-10-18 12:00:07 -0500

    Prueba de Heroku

--- reportesLivingNet/settings.py
+++ reportesLivingNet/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.herokuapp.com', 
+ALLOWED_HOSTS = ['livingn3t.herokuapp.com', 
                 '127.0.0.1']
 
 
