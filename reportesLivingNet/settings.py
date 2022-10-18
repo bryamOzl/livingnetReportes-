390eb9f6a70bf6c3f4a2ba88eb7c2b1adae2cb97
bryamozl <bryan95_19@hotmail.com>
2022-10-18 10:20:36 -0500

    Update Allowed Host

--- reportesLivingNet/settings.py
+++ reportesLivingNet/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.herokuapp.com', 
+ALLOWED_HOSTS = ['whispering-plateau-84454.herokuapp.com', 
                 '127.0.0.1']
 
 
